29339d6694280fad7f8b8a353b5bb13a8c8f8b00
Nikhil Mundra <nikhilmundra29@gmail.com>
2019-11-10 18:51:31 +0530

    change

--- samples/auto/balloon.py
+++ samples/auto/balloon.py
@@ -247,78 +247,6 @@ def train(model):
                 layers='heads')
 
 
-# def color_splash(image, mask):
-#     """Apply color splash effect.
-#     image: RGB image [height, width, 3]
-#     mask: instance segmentation mask [height, width, instance count]
-#
-#     Returns result image.
-#     """
-#     # Make a grayscale copy of the image. The grayscale copy still
-#     # has 3 RGB channels, though.
-#     gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
-#     # Copy color pixels from the original color image where mask is set
-#     if mask.shape[-1] > 0:
-#         # We're treating all instances as one, so collapse the mask into one layer
-#         mask = (np.sum(mask, -1, keepdims=True) >= 1)
-#         splash = np.where(mask, image, gray).astype(np.uint8)
-#     else:
-#         splash = gray.astype(np.uint8)
-#     return splash
-
-
-# def detect_and_color_splash(model, image_path=None, video_path=None):
-#     assert image_path or video_path
-#
-#     # Image or video?
-#     if image_path:
-#         # Run model detection and generate the color splash effect
-#         print("Running on {}".format(args.image))
-#         # Read image
-#         image = skimage.io.imread(args.image)
-#         # Detect objects
-#         r = model.detect([image], verbose=1)[0]
-#         # Color splash
-#         splash = color_splash(image, r['masks'])
-#         # Save output
-#         file_name = "splash_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
-#         skimage.io.imsave(file_name, splash)
-#     elif video_path:
-#         import cv2
-#         # Video capture
-#         vcapture = cv2.VideoCapture(video_path)
-#         width = int(vcapture.get(cv2.CAP_PROP_FRAME_WIDTH))
-#         height = int(vcapture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-#         fps = vcapture.get(cv2.CAP_PROP_FPS)
-#
-#         # Define codec and create video writer
-#         file_name = "splash_{:%Y%m%dT%H%M%S}.avi".format(datetime.datetime.now())
-#         vwriter = cv2.VideoWriter(file_name,
-#                                   cv2.VideoWriter_fourcc(*'MJPG'),
-#                                   fps, (width, height))
-#
-#         count = 0
-#         success = True
-#         while success:
-#             print("frame: ", count)
-#             # Read next image
-#             success, image = vcapture.read()
-#             if success:
-#                 # OpenCV returns images as BGR, convert to RGB
-#                 image = image[..., ::-1]
-#                 # Detect objects
-#                 r = model.detect([image], verbose=0)[0]
-#                 # Color splash
-#                 splash = color_splash(image, r['masks'])
-#                 # RGB -> BGR to save image to video
-#                 splash = splash[..., ::-1]
-#                 # Add image to video writer
-#                 vwriter.write(splash)
-#                 count += 1
-#         vwriter.release()
-#     print("Saved to ", file_name)
-
-
 ############################################################
 #  Training
 ############################################################
@@ -398,16 +326,16 @@ if __name__ == '__main__':
     else:
         weights_path = args.weights
 
-    # # Load weights
-    # print("Loading weights ", weights_path)
-    # if args.weights.lower() == "coco":
-    #     # Exclude the last layers because they require a matching
-    #     # number of classes
-    #     model.load_weights(weights_path, by_name=True, exclude=[
-    #         "mrcnn_class_logits", "mrcnn_bbox_fc",
-    #         "mrcnn_bbox", "mrcnn_mask"])
-    # else:
-    #     model.load_weights(weights_path, by_name=True)
+    # Load weights
+    print("Loading weights ", weights_path)
+    if args.weights.lower() == "coco":
+        # Exclude the last layers because they require a matching
+        # number of classes
+        model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
+    else:
+        model.load_weights(weights_path, by_name=True)
 
     # Train or evaluate
     if args.command == "train":
